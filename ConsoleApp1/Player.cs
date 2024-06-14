6f5691819aa499682cf627579699def1b92137b5
S-JOBI23-062\student <231049@st.yoshida-g.ac.jp>
2024-06-14 12:39:43 +0900

    クラスの追加、削除 #2

---
+++ ConsoleApp1/Player.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Player
+    {
+        private string _name="miura";
+        private int _age=19;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello,World!");
+            Console.WriteLine("Hello,Hellomaru.");
+            Console.ReadLine();
+        }
+
+        private void CreatePlayer(string name,int age)
+        {
+            Player player = new Player();
+            player._name = name;
+            player._age = age;
+
+            Console.WriteLine(name + " " + player._age);
+
+            return;
+        }
+
+        private void ShowPlayer() 
+        {
+            CreatePlayer(_name, _age);
+
+            Console.WriteLine(_name);
+            Console.WriteLine(_age);
+            ShowPlayer();
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConsoleApp1;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,10 +9,12 @@ namespace ConsoleApp1
 {
     internal class Program
     {
-        static void Main(string[] args)
+        
+        /*static void Main(string[] args)
         {
-        }
+            Console.WriteLine("Hello,World!");
+            Console.WriteLine("Hello,Hellomaru.");
+            Console.ReadLine();
+        }*/
     }
-
-
 }
